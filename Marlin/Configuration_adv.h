b1a10db3a0187c0f73c408749e75e9722f82d02f
Mark <mdelecate@users.noreply.github.com>
2019-10-13 16:42:19 +0800

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1065,7 +1065,7 @@
   // Add an optimized binary file transfer mode, initiated with 'M28 B1'
   //#define BINARY_FILE_TRANSFER
 
-  #if TARGET_LPC1768 //keenmetal changed from HAS_SDCARD_CONNECTION
+  #if HAS_SDCARD_CONNECTION
     /**
      * Set this option to one of the following (or the board's defaults apply):
      *
