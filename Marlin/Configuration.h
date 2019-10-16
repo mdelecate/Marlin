9588b26960381f35d7e134175b6fdcadcc238fe9
Mark <mdelecate@users.noreply.github.com>
2019-10-17 02:46:09 +0800

    Update Configuration.h
    Inverted extruder stepper motor direction

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1034,7 +1034,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+#define INVERT_E0_DIR true //keenmetal edited for TMC 2209. Default for TMC2100 is the false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
