8b8cf7730556aefcca02ed906e143f8d92cf52b8
Mark <mdelecate@users.noreply.github.com>
2019-10-17 14:00:21 +0800

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -928,7 +928,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 27, -36, 0.33 } // keenmetal
+#define NOZZLE_TO_PROBE_OFFSET { 27, -36, -0.33 } // keenmetal
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 35
