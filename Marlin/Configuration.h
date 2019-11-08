c88f3a0f1ea7ee65d52750a331fa7b651e457666
Mark <mdelecate@users.noreply.github.com>
2019-11-08 12:23:50 +0800

    Update Configuration.h
    Calibrated DEFAULT_AXIS_STEPS_PER_UNIT

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -726,7 +726,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.121, 80.121, (399.778*4), 445 } //keenmetal - Artillery default values were: { 80.121, 80.121, 399.778, 445 } / default values were: { 80, 80, 4000, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.121, 80.521, 1607, 471 } //keenmetal - Artillery default values were: { 80.121, 80.121, 399.778, 445 } / default values were: { 80, 80, 4000, 500 }
 
 /**
  * Default Max Feed Rate (mm/s)
