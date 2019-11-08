87d1020c9e3e675423d39189c3c89fa9aa5988a5
Mark <mdelecate@users.noreply.github.com>
2019-11-08 10:58:32 +0800

    Update Configuration.h
    Updated NOZZLE_TO_PROBE_OFFSET and JUNCTION_DEVIATION_MM and DEFAULT_MAX_ACCELERATION

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -733,7 +733,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 12.5, 40 } //keenmetal - default values were: { 300, 300, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 20, 40 } //keenmetal - default values were: { 300, 300, 5, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -746,7 +746,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 4800, 4800, 100, 10000 } //keenmetal - default values were: { 3000, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 100, 10000 } //keenmetal - default values were: { 3000, 3000, 100, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -795,7 +795,7 @@
  *   http://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.05 // (mm) Distance from real junction edge //keenmetal - default value: 0.013
+  #define JUNCTION_DEVIATION_MM 0.045 // (mm) Distance from real junction edge //keenmetal - default value: 0.013
 #endif
 
 /**
@@ -929,7 +929,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 27, -36, -0.33 } // keenmetal
+#define NOZZLE_TO_PROBE_OFFSET { 27, -36, -0.7 } // keenmetal
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 35
