4eff33004dc405624dfe99647367e634ae8cb67f
Mark <mdelecate@users.noreply.github.com>
2019-11-08 14:49:00 +0800

    Update Configuration.h
    Adjusted NOZZLE_TO_PROBE_OFFSET to from Z-0.7 to Z-0.5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -929,7 +929,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 27, -36, -0.7 } // keenmetal
+#define NOZZLE_TO_PROBE_OFFSET { 27, -36, -0.5 } // keenmetal
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 35
