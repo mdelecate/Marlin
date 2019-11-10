c9c997f21c6805a5595307c7e7b9a311f84a5f9d
Mark <mdelecate@users.noreply.github.com>
2019-11-10 17:30:31 +0800

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1355,7 +1355,7 @@
 #define LIN_ADVANCE //keenmetal enabled
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
-  #define LIN_ADVANCE_K 0.13    // Unit: mm compression per 1mm/s extruder speed //keenmetal - default value was 0.22
+  #define LIN_ADVANCE_K 0.04    // Unit: mm compression per 1mm/s extruder speed //keenmetal - default value was 0.22
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
 #endif
 
