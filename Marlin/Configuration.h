9dcf8ec078943de4af69226a626a37a67e656203
Mark <mdelecate@users.noreply.github.com>
2019-11-10 17:31:10 +0800

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -761,7 +761,7 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          800    // X, Y, Z and E acceleration for printing moves //keenmetal - default value: 3000
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves //keenmetal - default value: 3000
 #define DEFAULT_RETRACT_ACCELERATION  10000    // E acceleration for retracts //keenmetal - default value: 3000
 #define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves //keenmetal - default value: 3000
 
